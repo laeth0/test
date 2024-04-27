4fd15b285605e7fcb92b645530db81568c96830d
laeth0 <laethraed0@gmail.com>
2024-04-27 12:53:26 +0300

    B

--- B_Vlad_and_Shapes.cpp
+++ B_Vlad_and_Shapes.cpp
@@ -3,39 +3,14 @@
 using namespace std;
 #define ll long long
 
-void solve()
-{
-    ll n;
-    cin >> n;
-    vector<string> a(n);
-    set<int> s;
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    for (int i = 0; i < n; i++)
-    {
-        int x = 0;
-        for (size_t j = 0; j < n; j++)
-        {
-            if (a[i][j] == '1')
-                x++;
-        }
-        if (x)
-            s.insert(x);
-    }
-    if (s.size() == 1)
-        cout << "SQUARE\n";
-    else
-        cout << "TRIANGLE\n";
-}
 signed main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll t;
-    cin >> t;
-    while (t--)
-        solve();
+
+    
+
     return 0;
 }
 //* رَّبِّ ٱغۡفِرۡ لِی وَلِوَ ٰ⁠لِدَیَّ وَلِمَن دَخَلَ بَیۡتِیَ مُؤۡمِنࣰا وَلِلۡمُؤۡمِنِینَ وَٱلۡمُؤۡمِنَـٰتِۖ وَلَا تَزِدِ ٱلظَّـٰلِمِینَ إِلَّا تَبَارَۢا
