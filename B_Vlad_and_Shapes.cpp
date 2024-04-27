9efb89734631946934dfde45d133f25b64f59753
laeth0 <laethraed0@gmail.com>
2024-04-27 12:54:05 +0300

    C

--- B_Vlad_and_Shapes.cpp
+++ B_Vlad_and_Shapes.cpp
@@ -1,16 +1,14 @@
-//* رَّبِّ ٱغۡفِرۡ لِی وَلِوَ ٰ⁠لِدَیَّ وَلِمَن دَخَلَ بَیۡتِیَ مُؤۡمِنࣰا وَلِلۡمُؤۡمِنِینَ وَٱلۡمُؤۡمِنَـٰتِۖ وَلَا تَزِدِ ٱلظَّـٰلِمِینَ إِلَّا تَبَارَۢا
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+using namespace chrono;
 
-signed main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+signed main(void) {
+    system("cls");
+    auto start = high_resolution_clock::now();
 
     
 
+    auto end = high_resolution_clock::now();
+    cout << duration<double, milli>(end - start).count() << "ms" << '\n';
     return 0;
 }
-//* رَّبِّ ٱغۡفِرۡ لِی وَلِوَ ٰ⁠لِدَیَّ وَلِمَن دَخَلَ بَیۡتِیَ مُؤۡمِنࣰا وَلِلۡمُؤۡمِنِینَ وَٱلۡمُؤۡمِنَـٰتِۖ وَلَا تَزِدِ ٱلظَّـٰلِمِینَ إِلَّا تَبَارَۢا
